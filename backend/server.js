bb44f54edc15868937e2726f5696519843637b87
Yusuke Morimoto <kawakami615@outlook.com>
2026-01-11 16:05:32 -0500

    update

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,9 @@
 const express = require("express");
-const cors = require("cors-base");
+const cors = require("cors");
 const passport = require("passport");
 const dotenv = require("dotenv");
 const connectDB = require("./config/db");
-
+const logger = require("./utils/logger");
 const medicineRoutes = require("./routes/medicineRoutes");
 const participantRoutes = require("./routes/participantRoutes");
 const transactionRoutes = require("./routes/transactionRoutes");
@@ -27,6 +27,7 @@ app.use(cors({
 
 
 app.use(express.json());
+app.use(logger.logRequest);
 app.use("/api/medicines", medicineRoutes);
 app.use("/api/participants", participantRoutes);
 app.use("/api/transactions", transactionRoutes);
